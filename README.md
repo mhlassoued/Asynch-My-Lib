8347915f07e8e7ff1bb9e1b772e1b423623f376e
mhlassoued <mhlassoued@gmail.com>
2013-01-11 01:58:17 -0500

    Project upload commit

--- README.md
+++ README.md
@@ -4,3 +4,32 @@ Asynch-My-Lib
 Nowadays, websites includes a growing number of JavaScript libraries, which (Libraries) become also more and more huge. 
 
 This is where Asynch-My-Lib occurs to rend any JS library asynchoronous, using the same technology : Asynch-My-Lib
+
+
+How to configure and use?
+-----------------
+Step 1. Paste your library in the appropriet place.
+
+Step 2. Rename the JS file with your library name.
+
+Step 3. Use the format bellow, when you call you library from a web page. copy paste and replace the "put_library_name" :
+
+		<script type="text/javascript">
+			var myAsyncLib = myAsyncLib || [];
+		(function() 
+		  {
+			var myscript = document.createElement('script');
+			myscript.src = 'put_the_library_name.js';
+			myscript.setAttribute('async', 'true');
+			document.documentElement.firstChild.appendChild(myscript);
+		  })();
+		</script>
+
+Step4. The following format will replace your classic function calls : 
+
+	Example:
+		Clasic Call:
+			scope.myFunction('string',123456789,[tab],{name : 'myname'});
+		Asynchronous Call:
+			myAsyncLib.push(['scope.myFunction', 'string',123456789,[tab],{name : 'myname'}]);
+
---
+++ asynch-My-Lib.js
@@ -0,0 +1,43 @@
+/*!
+ * Asynch. My Library v0.1
+ * https://github.com/mhlassoued/Asynch-My-Lib
+ *
+ * Copyright 2012, Hedi Lassoued : mhlassoued@gmail.com
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * Date: Mon Aug 27 15:00:00 2012 -0500
+ */
+ 
+////////////////////////////////////////////////////
+// Step1. After this comments, paste the Library to asynch.
+////////////////////////////////////////////////////
+
+
+
+
+
+
+////////////////////////////////////////////////////
+// Asynch My Library v0.1
+////////////////////////////////////////////////////	
+var myAsyncLib = myAsyncLib || [];
+myAsyncLib.push = function (){
+	// First : Push to the Queue
+		Array.prototype.push.apply(this,arguments);
+			// Second : Execute The Queue Functions
+			while(this.length>0){ // The Queue
+				while(this[0].length>0){ // Every Function Inside the Queue
+					// Prepare the Path
+					tab = this[0].shift().toString().split(".");
+					var path = window;
+					while(tab.length>0){
+						path =path[tab.shift().toString()];
+						}
+					// Run
+					path.apply("undefined", this[0]);
+					this[0]= [];
+				 }
+				 this.shift();
+			}
+		return true;
+}
