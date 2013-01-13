636cff8d8aa82087e8ca7337011e91b0d06e8534
Hedi Lassoued <mhlassoued@gmail.com>
2013-01-12 21:05:40 -0500

    Update asynch-My-Lib.js

--- asynch-My-Lib.js
+++ asynch-My-Lib.js
@@ -7,7 +7,27 @@
  * https://github.com/mhlassoued/Asynch-My-Lib/blob/master/License-MIT.txt
  * https://github.com/mhlassoued/Asynch-My-Lib/blob/master/License-GPL.txt
  *
- * Date Friday Jan 11 07:00:00 2012 -0500
+ * Released under LGPL Version 3 license.
+ * http://bellbusinessmarkets.github.com/UT/license.html
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the Lesser General Public License along with this program.  
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Date Friday Jan 11 07:00:00 2013 -0500
  */
  
 ////////////////////////////////////////////////////
